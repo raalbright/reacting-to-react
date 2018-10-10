6e52df43bf029137034e13a523484afea7603ebc
Ray Albright <ray.a.albright@gmail.com>
2018-10-10 18:15:36 -0500

    completed step 4.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 
-const App = ({ name }) => {
-  return <h1>{`Hello ${name}`}</h1>;
-}
+// const App = ({ name }) => {
+//   return <h1>{`Hello ${name}`}</h1>;
+// }
+
+class App extends Component {
+
+  render (props) {
+    console.log(props);
+    return <h1>{ 'Hello ' + this.props.name}</h1>;
+  }
+ }
 
 export default App;
