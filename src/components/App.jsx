81f204dc0a485dbd91185ea38aa342cd8d28e7f2
Ray Albright <ray.a.albright@gmail.com>
2018-10-10 18:33:11 -0500

    completed step 7.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 // const App = ({ name }) => {
 //   return <h1>{`Hello ${name}`}</h1>;
@@ -9,9 +9,18 @@ class App extends Component {
     text: 'Hello'
   }
 
-  render (props) {
-    console.log(props);
-    return <h1>{ this.state.text + ' '  + this.props.name}</h1>;
+  handleEvent = event => {
+    this.setState({
+      text: event.target.value
+    });
+  }
+  
+
+  render () {
+    return (<Fragment>
+    <input type="text" placeholder={this.state.text} onChange={this.handleEvent}/>
+      <h1>{ this.state.text + ' '  + this.props.name}</h1>
+    </Fragment>);
   }
  }
 
